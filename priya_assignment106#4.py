7be054d4b596ae9b1cfec02cd3506f53b297354d
Jyotiprav <42555600+Jyotiprav@users.noreply.github.com>
2018-12-28 19:16:08 -0500

    Add files via upload

---
+++ priya_assignment106#4.py
@@ -0,0 +1,34 @@
+import turtle
+t=turtle.Turtle()
+t.pensize(10)
+t.color("grey")
+
+
+#Circle 1
+t.penup()
+t.goto(-150,0)
+t.pendown()
+t.circle(100)
+
+
+#Circle2
+t.penup()
+t.goto(-50,0)
+t.pendown()
+t.circle(100)
+
+
+#Circle 3
+t.penup()
+t.goto(50,0)
+t.pendown()
+t.circle(100)
+
+
+#Circle 4
+t.penup()
+t.goto(150,0)
+t.pendown()
+t.circle(100)
+
+
